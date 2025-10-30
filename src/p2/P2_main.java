632a6ce4d6f88f305a985e4bfe9efed51d8a48a4
abc <bozorgmehrkazemieh@gmail.com>
2025-10-30 21:54:42 +0100

    ü2P3

---
+++ src/p2/P2_main.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class P2_main {
+
+    public static void main(String[] args) {
+        int i=7;
+        boolean b=false;
+
+        if(i>10){
+            b=true;
+        }
+
+    }
+
+}
---
+++ src/p3/P3_main.java
@@ -0,0 +1,20 @@
+package p3;
+
+public class P3_main {
+
+    public static void main(String[] args) {
+        int i=5;
+        int j=3000;
+        int k=-3;
+
+        if (i<j){
+            System.out.println("A");
+        }
+        if (i<k){
+            System.out.println("B");
+        }
+        if (j<k){
+            System.out.println("C");
+        }
+    }
+}
